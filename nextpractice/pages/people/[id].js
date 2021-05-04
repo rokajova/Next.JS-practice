30ac354d4aec6de117b36ce2364cc896a3e5ee78
rokajova <35878122+rokajova@users.noreply.github.com>
2021-05-04 18:50:03 +0300

    13

--- nextpractice/pages/people/[id].js
+++ nextpractice/pages/people/[id].js
@@ -2,17 +2,36 @@ export const getStaticPaths = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
+  // map data to an array of path objects with params (id)
   const paths = data.map((person) => {
-    return { params: { id: person.id.toString() } };
+    return {
+      params: { id: person.id.toString() },
+    };
   });
 
-  return { paths: paths, fallback: false };
+  return {
+    paths,
+    fallback: false,
+  };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { person: data },
+  };
+};
+
+const Details = ({ person }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{person.name}</h1>
+      <p>{person.email}</p>
+      <p>{person.website}</p>
+      <p>{person.address.city}</p>
     </div>
   );
 };
